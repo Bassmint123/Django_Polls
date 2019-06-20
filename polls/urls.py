152f72f986da54d382122b96f5d9bf398a391290
Bassmint123 <bradb@cimstrat.com>
2019-06-19 22:38:33 -0500

    Additions for 6/18

--- polls/urls.py
+++ polls/urls.py
@@ -2,13 +2,16 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-    # ex: /polls/
     path('', views.index, name='index'),
-    # ex: /polls/5/
+    # If you want to change the URL of the polls detail view to something else, 
+    # perhaps to something like polls/specifics/12/ instead of doing it in the 
+    # template (or templates) you would change it in polls/urls.py:
+    # added the word 'specifics'
+    # path('specifics/<int:question_id>/', views.detail, name='detail'),
     path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
+    # The 'name' value as called by the {% url %} template tag
     path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
