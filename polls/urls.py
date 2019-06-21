c315b521ead68d6cdea7b4732b2525eaeee128c3
Bassmint123 <bradb@cimstrat.com>
2019-06-20 22:09:50 -0500

    updated with generic views

--- polls/urls.py
+++ polls/urls.py
@@ -4,14 +4,8 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'),
-    # If you want to change the URL of the polls detail view to something else, 
-    # perhaps to something like polls/specifics/12/ instead of doing it in the 
-    # template (or templates) you would change it in polls/urls.py:
-    # added the word 'specifics'
-    # path('specifics/<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/', views.detail, name='detail'),
-    # The 'name' value as called by the {% url %} template tag
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
