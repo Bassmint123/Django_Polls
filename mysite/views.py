5b4793da5d846300f3ddfda8e9cb79ae0fd391b9
Bassmint123 <bradb@cimstrat.com>
2019-06-23 16:00:14 -0500

    Fixed the header navigation

--- mysite/views.py
+++ mysite/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-
+# This is an example of a function based view
 def index(request):
     template_name = "index.html"
     context = {}
     return render(request, template_name, context=context)
 
-
+# This is an example of a class based view
 class AboutPageView(TemplateView):
     template_name = 'about.html'
